eaf27ba5d7f273df349b4572bcfd0465277e5550
Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2025-11-18 11:35:07 -0300

    feat: mapeia rotas e adiciona handles a User

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver v1.5.0 // indirect
 	github.com/Masterminds/sprig v2.22.0+incompatible // indirect
+	github.com/go-chi/chi/v5 v5.2.3
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/huandu/xstrings v1.5.0 // indirect
 	github.com/imdario/mergo v0.3.16 // indirect
---
+++ internal/api/user_handlers.go
@@ -0,0 +1,15 @@
+package api
+
+import "net/http"
+
+func (api *Api) handleSignupUser( w http.ResponseWriter, r *http.Request){
+	panic("TODO - Não implementado")
+}
+
+func (api *Api) handleLoginUser( w http.ResponseWriter, r *http.Request){
+	panic("TODO - Não implementado")
+}
+
+func (api *Api) handleLogoutUser( w http.ResponseWriter, r *http.Request){
+	panic("TODO - Não implementado")
+}
